412296b0938c8efe1fbc7258fc63d89e082ec068
תמר פלד <Tamar-Peled>
2024-12-05 20:01:18 +0200

    Comments1

--- MyString.java
+++ MyString.java
@@ -21,7 +21,7 @@ public class MyString {
         String lowerCase = "";
             
         for (int i = 0; i < str.length(); i++){
-            if (str.charAt(i) >= 65 && str.charAt(i)<= 90) {
+            if (str.charAt(i) >= 'A' && str.charAt(i)<= 'Z') {
                 lowerCase += (char) (str.charAt(i) + 32);
             }else{
                 lowerCase += str.charAt(i);
